3389dfb6e7744198bbacb95f32de9b68f7d11590
Jwalin Joshi <jwalinjoshi@MacBook-Pro.local>
2021-10-24 03:02:47 -0700

    fix: fee in position mapping

--- contracts/NonfungiblePositionManager.sol
+++ contracts/NonfungiblePositionManager.sol
@@ -178,7 +178,8 @@ contract NonfungiblePositionManager is
         return Position({
         nonce: nonce,
         operator: address(this),
-        poolId: 0, 
+        poolId: 1, 
+        fee: fee,
         tickLower: tickLower,
         tickUpper: tickUpper, 
         liquidity: liquidity, 
@@ -335,65 +336,6 @@ contract NonfungiblePositionManager is
         itemIds[itemIdToIndex[tokenId]] = itemIds[itemIds.length - 1]; 
         itemIds.pop();
         UniswapNFTManager.safeTransferFrom(address(this), owner, tokenId);
-
-    }
-    function collect(INonfungiblePositionManager.CollectParams calldata params)
-        external
-        payable
-        returns (uint256 amount0, uint256 amount1)
-    {
-        require(params.amount0Max > 0 || params.amount1Max > 0);
-        // allow collecting to the nft position manager address with address 0
-        address recipient = params.recipient == address(0) ? address(this) : params.recipient;
-        Position storage position = _positions[params.tokenId];
-        IUniswapV3Pool pool = IUniswapV3Pool(itemIdToPoolAddrs[params.tokenId]);
-
-        (uint128 tokensOwed0, uint128 tokensOwed1) = (position.tokensOwed0, position.tokensOwed1);
-
-        // trigger an update of the position fees owed and fee growth snapshots if it has any liquidity
-        if (position.liquidity > 0) {
-            pool.burn(position.tickLower, position.tickUpper, 0);
-            (, uint256 feeGrowthInside0LastX128, uint256 feeGrowthInside1LastX128, , ) =
-                pool.positions(PositionKey.compute(address(this), position.tickLower, position.tickUpper));
-
-            tokensOwed0 += uint128(
-                FullMath.mulDiv(
-                    feeGrowthInside0LastX128 - position.feeGrowthInside0LastX128,
-                    position.liquidity,
-                    FixedPoint128.Q128
-                )
-            );
-            tokensOwed1 += uint128(
-                FullMath.mulDiv(
-                    feeGrowthInside1LastX128 - position.feeGrowthInside1LastX128,
-                    position.liquidity,
-                    FixedPoint128.Q128
-                )
-            );
-
-            position.feeGrowthInside0LastX128 = feeGrowthInside0LastX128;
-            position.feeGrowthInside1LastX128 = feeGrowthInside1LastX128;
-        }
-
-        // compute the arguments to give to the pool#collect method
-        (uint128 amount0Collect, uint128 amount1Collect) =
-            (
-                params.amount0Max > tokensOwed0 ? tokensOwed0 : params.amount0Max,
-                params.amount1Max > tokensOwed1 ? tokensOwed1 : params.amount1Max
-            );
-
-        // the actual amounts collected are returned
-        (amount0, amount1) = pool.collect(
-            recipient,
-            position.tickLower,
-            position.tickUpper,
-            amount0Collect,
-            amount1Collect
-        );
-
-        // sometimes there will be a few less wei than expected due to rounding down in core, but we just subtract the full amount expected
-        // instead of the actual amount so we can burn the token
-        (position.tokensOwed0, position.tokensOwed1) = (tokensOwed0 - amount0Collect, tokensOwed1 - amount1Collect);
 
     }
 }
